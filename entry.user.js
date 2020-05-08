ba666fb733c83b046f0a01105e453e2c0c0f2ddc
YY <z3388638@gmail.com>
2020-05-09 01:31:36 +0800

    Update meta

--- entry.user.js
+++ entry.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         cakeresume-html-editor
 // @namespace    https://2yc.tw
-// @version      0.1.1
+// @version      0.1.2
 // @description  A user friendly HTML editor to be plugged to CakeResume to easily tweak your resume layout
 // @author       Y.Y.
 // @match        https://www.cakeresume.com/resumes/*/edit
 // @license      MIT
 // @homepage     https://github.com/x3388638/cakeresume-html-editor
-// @updateURL
+// @updateURL    https://openuserjs.org/meta/x3388638/cakeresume-html-editor.js
 // @grant        none
 // ==/UserScript==
 $('body').append(
