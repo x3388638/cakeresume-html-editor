f769468cbddeb73293716ea1e46f44c9178db1a8
YY <z3388638@gmail.com>
2021-09-14 16:41:45 +0800

    update entry

--- entry.user.js
+++ entry.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         cakeresume-html-editor
 // @namespace    https://2yc.tw
-// @version      0.1.5
+// @version      0.1.6
 // @description  A user friendly HTML editor to be plugged to CakeResume to easily tweak your resume layout
 // @author       Y.Y.
 // @match        https://www.cakeresume.com/resumes/*/edit
@@ -12,6 +12,6 @@
 // ==/UserScript==
 $('body').append(
   $(
-    '<script src="https://rawcdn.githack.com/x3388638/cakeresume-html-editor/6d6a46e57dce0ae10d38285c8fef53f620a570ec/index.user.js"></script>'
+    '<script src="https://rawcdn.githack.com/x3388638/cakeresume-html-editor/cc95bd1b76d1a1e288e37b9b57de9e636f855c49/index.user.js"></script>'
   )
 )
